cead8844291f21ce672addc3fde464dac890deaf
evilMorty <michele.xiloyannis@akina.health>
2022-08-21 21:27:38 +0200

    Added event handler for falling Morty

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,12 @@ export default function Home() {
     animate();
 
     function init() {
-      character_container = document.getElementById('character_div');
+      character_container = document.getElementById('morty_div');
       let container_height = character_container.clientHeight;
       let container_width = character_container.clientWidth;
 
       camera = new THREE.PerspectiveCamera(45, container_width / container_height, 1, 2000);
-      camera.position.set(-1800, 200, 400);
+      camera.position.set(-1400, 0, 400);
 
       scene = new THREE.Scene();
 
@@ -45,6 +45,10 @@ export default function Home() {
       loader.load('/assets/three_models/Falling_Morty.fbx', function (object) {
         mixer = new THREE.AnimationMixer(object);
         const action = mixer.clipAction(object.animations[0]);
+        mixer.addEventListener('finished', function (e) {
+          document.getElementById('morty_div').style.visibility = 'hidden'
+        });
+        action.setLoop(THREE.LoopOnce);
         action.play();
         object.traverse(function (child) {
           if (child.isMesh) {
@@ -67,7 +71,7 @@ export default function Home() {
         controls.screenSpacePanning = true;
         controls.minDistance = 200;
         controls.maxDistance = 400;
-        controls.target.set(0, 100, 0);
+        controls.target.set(0, 200, 20);
         controls.update();
       }
       window.addEventListener('resize', onWindowResize);
@@ -82,8 +86,8 @@ export default function Home() {
     }
 
     function onWindowResize() {
-      let container_height = document.getElementById('character_div').clientHeight;
-      let container_width = document.getElementById('character_div').clientWidth;
+      let container_height = document.getElementById('morty_div').clientHeight;
+      let container_width = document.getElementById('morty_div').clientWidth;
       camera.aspect = container_width / container_height;
       camera.updateProjectionMatrix();
 
@@ -95,7 +99,8 @@ export default function Home() {
   return (
     <Layout>
       <main className="relative h-screen w-screen bg-slate-900">
-        <div id="character_div" className="absolute w-1/6 h-1/2 bottom-14 left-0 rounded-full" />
+        <div id="morty_div" className="absolute w-1/5 h-1/2 bottom-14 left-0 rounded-full" />
+        <div id="rick_div" className="absolute w-1/3 h-1/2 bottom-14 right-0 rounded-full" />
         <div
           id="personal_info"
           className="absolute h-10 aspect-square content-center rounded-xl left-4 top-4"
@@ -104,7 +109,9 @@ export default function Home() {
         >
           {isShown && (
             <div className="absolute w-72 bg-slate-800 text-left rounded-xl left-10 top-10 py-4 px-4">
-              Roboticist, web developer and data scientist wannabe. <br />
+              Roboticist. <br />
+              Web developer wannabe. <br />
+              Data scientist wannabe. <br />
               Rick and Morty fan. <br />
             </div>
           )}

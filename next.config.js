f7b2bc66144435bf9f300d23372193dc4733afd4
evilMorty <michele.xiloyannis@akina.health>
2022-08-23 00:20:37 +0200

    Next config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  nextConfig,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+}

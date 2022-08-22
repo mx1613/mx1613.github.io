e073ab9e5b0144f1a3eeabc7928e29f7b66b4756
evilMorty <michele.xiloyannis@akina.health>
2022-08-22 22:42:24 +0200

    Rick and morty components

--- pages/index.js
+++ pages/index.js
@@ -1,119 +1,19 @@
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import * as THREE from 'three';
+import React, { useState } from 'react';
 
 import { Layout } from '../components/layouts/layout'
-import { FBXLoader } from '/utils/three/jsm/loaders/FBXLoader.js'
-import { OrbitControls } from '/utils/three/jsm/controls/OrbitControls.js';
-
+import { Morty } from '../components/characters/morty';
+import { Rick } from '../components/characters/rick';
 
 export default function Home() {
   const [isShown, setIsShown] = useState(false);
 
-  useEffect(() => {
-    let camera, scene, renderer, character_container, controls, container_height, container_width;
-    const clock = new THREE.Clock();
-    let mixer;
-
-    init();
-    animate();
-
-    function init() {
-
-      initContainer();
-      initCamera();
-      initRenderer();
-
-      function initContainer() {
-        character_container = document.getElementById('morty_div');
-        container_height = character_container.clientHeight;
-        container_width = character_container.clientWidth;
-      }
-
-      function initCamera() {
-        camera = new THREE.PerspectiveCamera(45, container_width / container_height, 1, 2000);
-        camera.position.set(-1400, 0, 400);
-
-        scene = new THREE.Scene();
-
-        const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
-        scene.add(hemiLight);
-
-        const dirLight = new THREE.DirectionalLight(0xffffff, 1);
-        dirLight.position.set(0, 1000, 1000);
-        scene.add(dirLight);
-
-
-        const spotLight = new THREE.SpotLight(0xffffff);
-        spotLight.position.set(0, 1000, 1000);
-        scene.add(spotLight);
-
-        const pointLight = new THREE.PointLight(0xffffff, 1, 0);
-        pointLight.position.set(0, 1000, 1000);
-        scene.add(pointLight);
-
-      }
-
-      function initRenderer() {
-        const loader = new FBXLoader();
-        loader.load('/assets/three_models/Falling_Morty.fbx', function (object) {
-          mixer = new THREE.AnimationMixer(object);
-          const action = mixer.clipAction(object.animations[0]);
-          mixer.addEventListener('finished', function (e) {
-            document.getElementById('morty_div').style.visibility = 'hidden'
-          });
-          action.setLoop(THREE.LoopOnce);
-          action.play();
-          object.traverse(function (child) {
-            if (child.isMesh) {
-              child.castShadow = true;
-              child.receiveShadow = true;
-            }
-          });
-
-          scene.add(object);
-        });
-
-        renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(container_width, container_height);
-        renderer.setClearColor(0x000000, 0);
-
-        // Not sure why this runs twice...
-        if (character_container.childElementCount === 0) {
-          character_container.appendChild(renderer.domElement);
-          controls = new OrbitControls(camera, renderer.domElement);
-          controls.screenSpacePanning = true;
-          controls.minDistance = 200;
-          controls.maxDistance = 400;
-          controls.target.set(0, 220, 20);
-          controls.update();
-        }
-        window.addEventListener('resize', onWindowResize);
-      }
-    }
-
-    function animate() {
-      requestAnimationFrame(animate);
-      const delta = clock.getDelta();
-      if (mixer) mixer.update(delta);
-      renderer.render(scene, camera);
-    }
-
-    function onWindowResize() {
-      let container_height_lc = document.getElementById('morty_div').clientHeight;
-      let container_width_lc = document.getElementById('morty_div').clientWidth;
-      camera.aspect = container_width_lc / container_height_lc;
-      camera.updateProjectionMatrix();
-      renderer.setSize(container_width_lc, container_height_lc);
-    }
-  }, []);
-
   return (
     <Layout>
       <main className="relative h-screen w-screen bg-slate-900">
-        <div id="morty_div" className="absolute w-1/5 h-1/2 bottom-14 left-0 rounded-full" />
-        <div id="rick_div" className="absolute w-1/3 h-1/2 bottom-14 right-0 rounded-full" />
+        <Rick className="absolute w-1/4 h-1/2 bottom-14 right-0 rounded-full" />
+        <Morty className="absolute w-1/4 h-1/2 bottom-14 left-0 rounded-full" />
+
         <div
           id="personal_info"
           className="absolute h-10 aspect-square content-center rounded-xl left-4 top-4"
@@ -130,6 +30,7 @@ export default function Home() {
           )}
           <Image src="/icons/info.svg" layout="fill" />
         </div>
+
       </main>
     </Layout>
   )

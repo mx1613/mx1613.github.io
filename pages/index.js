9f6e62e269f01e653b7f102788985755bb839dc0
evilMorty <michele.xiloyannis@akina.health>
2022-08-22 09:19:09 +0200

    Header and logos

--- pages/index.js
+++ pages/index.js
@@ -129,16 +129,24 @@ export default function Home() {
           <Image src="/icons/info.svg" layout="fill" />
         </div>
         <div className="absolute w-1/5 py-4 rounded-bl-lg bg-slate-800 flex justify-between top-0 right-0 ">
-          <div className="px-4 flex gap-2">
-            <Image src="/icons/github-logo.svg" width={30} height={30} />
-            <Link href="https://github.com/mx1613/michele.info">
-              Source Code
+          <div className="px-4  gap-2">
+            <Link href="https://scholar.google.com/citations?user=e8gAwugAAAAJ&hl=en">
+              <Image src="/icons/scholar.svg" width={30} height={30} />
             </Link>
           </div>
           <div className="px-4 flex gap-2">
-            <Image src="/icons/github-logo.svg" width={30} height={30} />
             <Link href="https://github.com/mx1613/michele.info">
-              Source Code
+              <Image src="/icons/github-logo.svg" width={30} height={30} />
+            </Link>
+          </div>
+          <div className="px-4 flex gap-2">
+            <Link href="https://www.linkedin.com/in/michelexiloyannis/">
+              <Image src="/icons/linkedin.svg" width={30} height={30} />
+            </Link>
+          </div>
+          <div className="px-4 flex gap-2">
+            <Link href="https://medium.com/@m.xiloyannis">
+              <Image src="/icons/medium.svg" width={30} height={30} />
             </Link>
           </div>
         </div>

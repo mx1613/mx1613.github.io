08b597373fd9198326ae0b73fb4a79e965ba88eb
evilMorty <michele.xiloyannis@akina.health>
2022-08-22 23:54:52 +0200

    README

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# michele.info
-Personal website
+# Page description 
+This is a single-pager  that displays some basic infomration about myself. It is written with NextJS and with the help of Tailwind for styling. 
+
+I used it as a chance to play around with the THREE.JS library, to animate Rick (and Morty, in the source code).
 
 
-
-
+### Réddite quae sunt Caésaris Caésari  
 
 Rick Sanchez - Ready for UE4 Rigged by Exo404 is licensed under Creative Commons Attribution
 Morty - Ready for UE4 Rigged by Exo404 is licensed under Creative Commons Attribution
--- components/header/header.js
+++ components/header/header.js
@@ -5,23 +5,31 @@ export function Header() {
     return (
         <div className="absolute w-64 py-4 rounded-bl-lg bg-slate-800 flex justify-between top-0 right-0 ">
             <div className="px-4  gap-2" style={{ cursor: 'pointer' }}>
-                <Link href="https://scholar.google.com/citations?user=e8gAwugAAAAJ&hl=en" passHref>
-                    <Image src="/icons/scholar.svg" alt="Scholar icon" width={30} height={30} />
+                <Link href="https://scholar.google.com/citations?user=e8gAwugAAAAJ&hl=en">
+                    <a>
+                        <Image src="/icons/scholar.svg" alt="Scholar icon" width={30} height={30} />
+                    </a>
                 </Link>
             </div>
             <div className="px-4 flex gap-2" style={{ cursor: 'pointer' }}>
                 <Link href="https://github.com/mx1613/michele.info" passHref>
-                    <Image src="/icons/github-logo.svg" alt="Github icon" width={30} height={30} />
+                    <a>
+                        <Image src="/icons/github-logo.svg" alt="Github icon" width={30} height={30} />
+                    </a>
                 </Link>
             </div>
             <div className="px-4 flex gap-2" style={{ cursor: 'pointer' }}>
                 <Link href="https://www.linkedin.com/in/michelexiloyannis/" passHref>
-                    <Image src="/icons/linkedin.svg" alt="Linkedin icon" width={30} height={30} />
+                    <a>
+                        <Image src="/icons/linkedin.svg" alt="Linkedin icon" width={30} height={30} />
+                    </a>
                 </Link>
             </div>
             <div className="px-4 flex gap-2" style={{ cursor: 'pointer' }}>
                 <Link href="https://medium.com/@m.xiloyannis" passHref>
-                    <Image src="/icons/medium.svg" alt="Medium icon" width={30} height={30} />
+                    <a>
+                        <Image src="/icons/medium.svg" alt="Medium icon" width={30} height={30} />
+                    </a>
                 </Link>
             </div>
         </div>

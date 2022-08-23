4ae6f0cb15be607346d723e461b35c241507e3bf
evilMorty <michele.xiloyannis@akina.health>
2022-08-23 23:53:40 +0200

    Responsive font size

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,11 +5,11 @@ export function Footer() {
     return (
         <footer className="absolute w-screen rounded-t-lg bg-slate-800 py-4 flex flex-row justify-between bottom-0 shadow-lg">
             <div className="right-0 px-4">
-                &copy; {new Date().getFullYear()} Copyright Michele Xiloyannis
+                <p className='md:text-sm '>&copy; {new Date().getFullYear()} Michele Xiloyannis</p>
             </div>
             <div className=" px-4 flex gap-2" style={{ cursor: 'pointer' }}>
                 <Link href="https://github.com/mx1613/michele.info">
-                    Page source code
+                    <p className='md:text-sm '>Page src code</p>
                 </Link>
             </div>
         </footer>

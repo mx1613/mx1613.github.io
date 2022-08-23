39857e439c02b9b3bbe4cd39dec35898d7e694b8
evilMorty <michele.xiloyannis@akina.health>
2022-08-23 22:51:46 +0200

    Config for deployment and nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
-// {
-//   // Use the CDN in production and localhost for development.
-//   nextConfig,
-//   assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
-// }
+module.exports = {
+  assetPrefix: isProd ? '/mx1613.github.io' : ''
+}

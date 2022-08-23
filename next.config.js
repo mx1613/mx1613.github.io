d847cd6d2df3b8f95473e47fbd8bc628aa390b72
evilMorty <michele.xiloyannis@akina.health>
2022-08-23 23:10:12 +0200

    Changed config and actions back to nextjs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/mx1613.github.io' : ''
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
+
+
+module.exports = nextConfig

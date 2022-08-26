528e7c57bebeb87989e277fc5a5a1901f4fc4aab
evilMorty <michele.xiloyannis@akina.health>
2022-08-26 20:19:27 +0200

    wall-e, ignore bfx

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Layout } from '../components/layouts/layout'
 import { Rick } from '../components/characters/rick';
+import { Walle } from '../components/characters/walle';
 import { InfoPanel } from '../components/info/infoPanel';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <Layout>
       <main className="relative h-screen w-screen bg-slate-900">
         <Rick className="absolute w-80 h-72 bottom-14 right-0 rounded-full" />
+        <Walle className="absolute w-80 h-72 bottom-14 left-0 rounded-full" />
         <InfoPanel />
       </main>
     </Layout>

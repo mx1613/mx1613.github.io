eb969bd2397179c374acd50104cf1a3cb71a6cb3
evilMorty <michele.xiloyannis@akina.health>
2022-08-26 20:30:31 +0200

    Refactroring

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, from 'react';
 import { Layout } from '../components/layouts/layout'
 import { Rick } from '../components/characters/rick';
 import { Walle } from '../components/characters/walle';
@@ -5,11 +6,16 @@ import { InfoPanel } from '../components/info/infoPanel';
 
 export default function Home() {
 
+
   return (
     <Layout>
       <main className="relative h-screen w-screen bg-slate-900">
-        <Rick className="absolute w-80 h-72 bottom-14 right-0 rounded-full" />
-        <Walle className="absolute w-80 h-72 bottom-14 left-0 rounded-full" />
+        <Rick
+          className="absolute w-80 h-72 bottom-14 right-0 rounded-full"
+        />
+        <Walle
+          className="absolute w-80 h-72 bottom-14 left-0 rounded-full"
+        />
         <InfoPanel />
       </main>
     </Layout>

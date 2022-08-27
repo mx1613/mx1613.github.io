1e1915f6e64ff0928424279e3088ba536dba33d3
evilMorty <michele.xiloyannis@akina.health>
2022-08-27 19:02:48 +0200

    Button and states for light and dark states

--- components/info/infoPanel.js
+++ components/info/infoPanel.js
@@ -2,18 +2,21 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 
 
-export function InfoPanel() {
+export function InfoPanel({ isDarkMode }) {
     const [isShown, setIsShown] = useState(false);
 
     return (
-        <div className="w-screen h-screen flex items-center justify-center " >
+        <div className="w-screen h-screen flex items-center justify-center  " >
             <div
                 id="personal_info"
-                className="absolute h-10 aspect-square content-center rounded-full shadow-lg left-4 top-4"
+                className="absolute h-10 aspect-square content-center rounded-full shadow-lg left-4 top-4 bg-slate-800"
                 onMouseEnter={() => setIsShown(true)}
                 onMouseLeave={() => setIsShown(false)}
             >
-                <Image src="/icons/info.svg" alt="Info icon" layout="fill" />
+                {isDarkMode ?
+                    <Image src="/icons/info-dark.svg" alt="Info icon" layout="fill" /> :
+                    <Image src="/icons/info-light.svg" alt="Info icon" layout="fill" />
+                }
             </div>
             {isShown && (
                 <div className="absolute w-fit bg-slate-800 text-left rounded-xl py-4 px-4">

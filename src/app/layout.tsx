178323fbbfec470db6ecdabc96bdf35b15ae0b07
Michele <michele.xiloyannis@akina.health>
2024-08-02 18:49:32 +0200

    chore: fixed tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Footer } from "../components/footer/footer";
+import { Header } from "../components/header/header";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });

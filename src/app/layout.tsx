a899f0cc3f9a76c77e9b09e14d457586f9f02b9d
Michele <michele.xiloyannis@akina.health>
2024-08-04 17:14:01 +0200

    style: prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Footer } from "../components/footer/footer";
-import { Header } from "../components/header/header";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Michele Info",
-  description: "",
+  title: "Michele",
+  description: "Personal website - Michele Xiloyannis",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import ThreeDScene from "../components/characters/scene";
 import { Footer } from "../components/footer/footer";
 import { Header } from "../components/header/header";
@@ -9,10 +8,6 @@ import { InfoPanel } from "../components/info/infoPanel";
 export default function Home() {
   return (
     <main className="relative h-screen w-screen bg-slate-900 flex items - center justify - center">
-      <Head>
-        <meta name="description" content={"Info@Michele"} />
-        <title>{"A basic intro to myself"}</title>
-      </Head>
       <ThreeDScene className="absolute inset-0" />
       <div className="absolute top-0 2 m-4"></div>
       <InfoPanel />
